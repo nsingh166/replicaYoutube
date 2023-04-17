d1956d9173f2de64c067258bcf976b850be9f981
nsingh166 <navjotsingh88888@gmail.com>
2023-04-16 22:22:15 -0700

    Milestone 1

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]" });
+// localhost:3000
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'CSC 317 App',
+    name: 'Navjot SIngh',
+  });
 });
+
+
 
 module.exports = router;
